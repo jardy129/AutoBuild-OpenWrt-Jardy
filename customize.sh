7db3ad60660705c3ee11dfc4b465f6ef8acd2052
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-04-15 10:25:32 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -13,8 +13,8 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
-echo 'src-git luci-theme-edge https://github.com/kiddin9/luci-theme-edge.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git serverchan https://github.com/tty228/luci-app-serverchan.git' >>feeds.conf.default
-echo 'src-git theme-edge https://github.com/garypang13/luci-theme-edge.git' >>feeds.conf.default
+#echo 'src-git luci-theme-edge https://github.com/kiddin9/luci-theme-edge.git' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git serverchan https://github.com/tty228/luci-app-serverchan.git' >>feeds.conf.default
+#echo 'src-git theme-edge https://github.com/garypang13/luci-theme-edge.git' >>feeds.conf.default
