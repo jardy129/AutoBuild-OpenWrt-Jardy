dd9950e90a60c00ca23aeae12df2b8ead389301e
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-04-15 13:35:35 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,7 +12,7 @@ sed -i 's/192.168.1.1/192.168.2.5/g' openwrt/package/base-files/files/bin/config
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-rm openwrt/package/lean/luci-theme-argon -rf
+#rm openwrt/package/lean/luci-theme-argon -rf
 #echo 'src-git luci-theme-edge https://github.com/kiddin9/luci-theme-edge.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
